e19060ad0d332d18716d53c7e166ef0ed2e32826
Jan Theiss <jantheiss@me.com>
2020-07-25 16:51:42 +0200

    bug fixes

--- ESP_SHD_ContactSensor.h
+++ ESP_SHD_ContactSensor.h
@@ -1,4 +1,7 @@
 #include "ESP_SmartHomeDevice.h"
+#include <SoftwareSerial.h>
+
+#define DEBUG 5
 
 #define MQTT_TOPIC_SIZE 70
 #define MQTT_PAYLOAD_BUFFER_SIZE 10
@@ -9,13 +12,13 @@ enum contactSensorType {
 };
 
 enum csStatus {
-  OPENED, CLOSED, ERROR
-}
+  CS_OPENED, CS_CLOSED, CS_ERROR
+};
 
 class ShdContactSensor : public ESP_SmartHomeDevice {
 public:
-  ShdContactSensor(contactSensorType _type, bool _sleepAfterPublish);
-  ShdContactSensor(contactSensorType _type, uint8_t _pin, bool _lowAcive, bool _sleepAfterPublish, uint16_t _intervalMillis);
+  ShdContactSensor(contactSensorType _type, bool _sleepAfterPublish, bool _getBatteryStatus);
+  ShdContactSensor(contactSensorType _type, uint8_t _pin, bool _lowAcive, bool _sleepAfterPublish, uint16_t _intervalMillis, bool _getBatteryStatus);
 private:
   static uint8_t contactSensorCount;
 
@@ -23,15 +26,25 @@ private:
   bool handleMqttRequest(char* _topic, unsigned char* _payload, uint16_t _length);
   void republish();
 
-  doorSensorType csType;
+  char payloadBuffer[50];
+
+  contactSensorType csType;
   char pubTopicState[MQTT_TOPIC_SIZE];
   csStatus lastStatus;
   bool sleepAfterPublish;
+  uint8_t csNumber;
+  bool getBatteryStatus;
+  char pubTopicBattery[MQTT_TOPIC_SIZE];
 
   uint8_t pin;
   bool lowActive;
   uint16_t updateInterval, updateCounter;
 
+  void getStatus();
   csStatus getStatusNormal();
   csStatus getStatusLsc();
-}
+
+  void lscCommunication(byte _cmd, byte _len, byte _value, uint8_t* _reply, uint8_t _maxReplySize, uint8_t* _replySize);
+
+  SoftwareSerial swSerial; // REMOVE!
+};
